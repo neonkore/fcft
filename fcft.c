c40bae247fe82c999c0ce876d1a4539ff11153ae
Daniel Eklöf <daniel@ekloef.se>
2019-12-04 21:41:43 +0100

    fcft: set max_x_advance to the width of the character ' '
    Some fonts (Noto Sans Mono) have font x_advance value that is way too
    large (possibly because they contain glyphs that are double width?)
    
    Try to load the glyph ' ' and use it's width. Only if that fails do we
    use the fonts max-x-advance value.

--- fcft.c
+++ fcft.c
@@ -392,6 +392,12 @@ from_font_set(FcPattern *pattern, FcFontSet *fonts, int start_idx,
     double descent = ft_face->size->metrics.descender / 64.;
     double ascent = ft_face->size->metrics.ascender / 64.;
 
+    /* Some fonts (Noto Sans Mono, for example) provides bad
+     * max_advance values. Use the width for ' ', if possible */
+    FT_UInt idx = FT_Get_Char_Index(font->face, L' ');
+    if ((ft_err = FT_Load_Glyph(font->face, idx, font->load_flags)) == 0)
+        max_x_advance = font->face->glyph->advance.x / 64. * font->pixel_size_fixup;
+
     font->public.height = ceil(height * font->pixel_size_fixup);
     font->public.descent = ceil(-descent * font->pixel_size_fixup);
     font->public.ascent = ceil(ascent * font->pixel_size_fixup);
