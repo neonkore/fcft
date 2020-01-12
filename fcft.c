neonkore/fcft

commit a17a9788b55a5a47615522e9d7c338e2430cb401

fcft: don't load fallback font if it doesn't have the char
When going through the fallback fonts, use FcCharSetHasChar() to check
if the font has the character *before* loading the font.

This should speed up the fallback search, and more importantly save
memory since we're not loading fonts we're not actually using into
memory.

fcft.c
@@ -194,7 +194,7 @@ underline_strikeout_metrics(struct font_priv *font)
 
 static bool
 from_font_set(FcPattern *pattern, FcFontSet *fonts, int start_idx,
-              struct font_priv *font, bool is_fallback)
+              struct font_priv *font, bool is_fallback, wchar_t must_have_char)
 {
     memset(font, 0, sizeof(*font));
 
@@ -211,6 +211,17 @@ from_font_set(FcPattern *pattern, FcFontSet *fonts, int start_idx,
                 FcPatternDestroy(pat);
                 continue;
             }
+        }
+
+        FcCharSet *charset;
+        if (must_have_char != -1 &&
+            FcPatternGetCharSet(pat, FC_CHARSET, 0, &charset) == FcResultMatch &&
+            !FcCharSetHasChar(charset, must_have_char))
+        {
+            LOG_DBG("%s: does not have %C (0x%04x), skipping",
+                    face_file, must_have_char, must_have_char);
+            FcPatternDestroy(pat);
+            continue;
         }
 
         final_pattern = pat;
@@ -430,7 +441,7 @@ from_font_set(FcPattern *pattern, FcFontSet *fonts, int start_idx,
 }
 
 static struct font_priv *
-from_name(const char *name, bool is_fallback)
+from_name(const char *name, bool is_fallback, wchar_t must_have_char)
 {
     LOG_DBG("instantiating %s%s", name, is_fallback ? " (fallback)" : "");
 
@@ -458,7 +469,7 @@ from_name(const char *name, bool is_fallback)
 
     struct font_priv *font = malloc(sizeof(*font));
 
-    if (!from_font_set(pattern, fonts, 0, font, is_fallback)) {
+    if (!from_font_set(pattern, fonts, 0, font, is_fallback, must_have_char)) {
         free(font);
         FcFontSetDestroy(fonts);
         FcPatternDestroy(pattern);
@@ -532,7 +543,7 @@ font_from_name(const char *names[], size_t count, const char *attributes)
         if (first) {
             first = false;
 
-            font = from_name(name, false);
+            font = from_name(name, false, -1);
             if (font == NULL)
                 return NULL;
 
@@ -595,7 +606,7 @@ glyph_for_wchar(const struct font_priv *font, wchar_t wc, struct glyph *glyph)
 
         tll_foreach(font->fallbacks, it) {
             if (it->item.font == NULL) {
-                it->item.font = from_name(it->item.pattern, true);
+                it->item.font = from_name(it->item.pattern, true, wc);
                 if (it->item.font == NULL)
                     continue;
             }
@@ -620,7 +631,7 @@ glyph_for_wchar(const struct font_priv *font, wchar_t wc, struct glyph *glyph)
             if (font->fc_loaded_fallbacks[i] == NULL) {
                 /* Load font */
                 struct font_priv *fallback = malloc(sizeof(*fallback));
-                if (!from_font_set(font->fc_pattern, font->fc_fonts, i, fallback, true))
+                if (!from_font_set(font->fc_pattern, font->fc_fonts, i, fallback, true, wc))
                 {
                     LOG_WARN("failed to load fontconfig fallback font");
                     free(fallback);

commit 13290fa5cb3e9ec3a5e801a685c5218631e90003

fcft: from_font_set: don't iterate the font set
Fail immediately if the indicated font cannot be loaded. It is up to
the caller to iterate the font list.

This fixes a regression introduced in
a17a9788b55a5a47615522e9d7c338e2430cb401, where we ignore fonts that
does not have the indicated glyph.

Doing so meant we now started iterating the font set list where we
previously had not. As a result, trying a user specified fallback font
could result in one **its** fallback fonts being loaded instead, which
is **not** what we wanted.

Previously, we would have loaded the user specified fallback font, and
then failed when trying to actually load the glyph.

fcft.c
@@ -193,47 +193,34 @@ underline_strikeout_metrics(struct font_priv *font)
 }
 
 static bool
-from_font_set(FcPattern *pattern, FcFontSet *fonts, int start_idx,
+from_font_set(FcPattern *pattern, FcFontSet *fonts, int font_idx,
               struct font_priv *font, bool is_fallback, wchar_t must_have_char)
 {
     memset(font, 0, sizeof(*font));
 
+    FcPattern *final_pattern = FcFontRenderPrepare(
+        NULL, pattern, fonts->fonts[font_idx]);
+    assert(final_pattern != NULL);
+
     FcChar8 *face_file = NULL;
-    FcPattern *final_pattern = NULL;
-    int font_idx = -1;
-
-    for (int i = start_idx; i < fonts->nfont; i++) {
-        FcPattern *pat = FcFontRenderPrepare(NULL, pattern, fonts->fonts[i]);
-        assert(pat != NULL);
-
-        if (FcPatternGetString(pat, FC_FT_FACE, 0, &face_file) != FcResultMatch) {
-            if (FcPatternGetString(pat, FC_FILE, 0, &face_file) != FcResultMatch) {
-                FcPatternDestroy(pat);
-                continue;
-            }
-        }
-
-        FcCharSet *charset;
-        if (must_have_char != -1 &&
-            FcPatternGetCharSet(pat, FC_CHARSET, 0, &charset) == FcResultMatch &&
-            !FcCharSetHasChar(charset, must_have_char))
-        {
-            LOG_DBG("%s: does not have %C (0x%04x), skipping",
-                    face_file, must_have_char, must_have_char);
-            FcPatternDestroy(pat);
-            continue;
-        }
-
-        final_pattern = pat;
-        font_idx = i;
-        break;
+    if (FcPatternGetString(final_pattern, FC_FT_FACE, 0, &face_file) != FcResultMatch &&
+        FcPatternGetString(final_pattern, FC_FILE, 0, &face_file) != FcResultMatch)
+    {
+        LOG_ERR("no face file path in pattern");
+        FcPatternDestroy(final_pattern);
+        return false;
     }
 
-    if (font_idx == -1)
+    FcCharSet *charset;
+    if (must_have_char != -1 &&
+        FcPatternGetCharSet(final_pattern, FC_CHARSET, 0, &charset) == FcResultMatch &&
+        !FcCharSetHasChar(charset, must_have_char))
+    {
+        LOG_DBG("%s: does not have %C (0x%04x)",
+                face_file, must_have_char, must_have_char);
+        FcPatternDestroy(final_pattern);
         return false;
-
-    assert(font_idx != -1);
-    assert(final_pattern != NULL);
+    }
 
     double dpi;
     if (FcPatternGetDouble(final_pattern, FC_DPI, 0, &dpi) != FcResultMatch)
@@ -628,23 +615,20 @@ glyph_for_wchar(const struct font_priv *font, wchar_t wc, struct glyph *glyph)
         assert(font->fc_pattern != NULL);
         assert(font->fc_fonts != NULL);
         assert(font->fc_loaded_fallbacks != NULL);
-        assert(font->fc_idx != -1);
+        assert(font->fc_idx == 0);
 
-        for (int i = font->fc_idx + 1; i < font->fc_fonts->nfont; i++) {
+        for (int i = 1; i < font->fc_fonts->nfont; i++) {
             if (font->fc_loaded_fallbacks[i] == NULL) {
                 /* Load font */
                 struct font_priv *fallback = malloc(sizeof(*fallback));
                 if (!from_font_set(font->fc_pattern, font->fc_fonts, i, fallback, true, wc))
                 {
-                    LOG_WARN("failed to load fontconfig fallback font");
                     free(fallback);
                     continue;
                 }
 
                 LOG_DBG("loaded new fontconfig fallback font");
-                assert(fallback->fc_idx >= i);
-
-                i = fallback->fc_idx;
+                assert(fallback->fc_idx == i);
                 font->fc_loaded_fallbacks[i] = fallback;
             }
 