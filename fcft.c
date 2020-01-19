2535f94c0a07e1e666be2174a3485f2f7d5759b2
Daniel Eklöf <daniel@ekloef.se>
2020-01-19 11:20:01 +0100

    fcft: FT_LOAD_DEFAULT is 0, so set it once

--- fcft.c
+++ fcft.c
@@ -314,7 +314,7 @@ from_font_set(FcPattern *pattern, FcFontSet *fonts, int font_idx,
     if (FcPatternGetInteger(final_pattern, FC_RGBA, 0, &fc_rgba) != FcResultMatch)
         fc_rgba = FC_RGBA_UNKNOWN;
 
-    int load_flags = 0;
+    int load_flags = FT_LOAD_DEFAULT;;
     if (!fc_antialias) {
         if (!fc_hinting || fc_hintstyle == FC_HINT_NONE)
             load_flags |= FT_LOAD_MONOCHROME | FT_LOAD_NO_HINTING | FT_LOAD_TARGET_NORMAL;
@@ -322,15 +322,15 @@ from_font_set(FcPattern *pattern, FcFontSet *fonts, int font_idx,
             load_flags |= FT_LOAD_MONOCHROME | FT_LOAD_TARGET_MONO;
     } else {
         if (!fc_hinting || fc_hintstyle == FC_HINT_NONE)
-            load_flags |= FT_LOAD_DEFAULT | FT_LOAD_NO_HINTING | FT_LOAD_TARGET_NORMAL;
+            load_flags |= FT_LOAD_NO_HINTING | FT_LOAD_TARGET_NORMAL;
         else if (fc_hinting && fc_hintstyle == FC_HINT_SLIGHT)
-            load_flags |= FT_LOAD_DEFAULT | FT_LOAD_TARGET_LIGHT;
+            load_flags |= FT_LOAD_TARGET_LIGHT;
         else if (fc_rgba == FC_RGBA_RGB || fc_rgba == FC_RGBA_BGR)
-            load_flags |= FT_LOAD_DEFAULT | FT_LOAD_TARGET_LCD;
+            load_flags |= FT_LOAD_TARGET_LCD;
         else if (fc_rgba == FC_RGBA_VRGB || fc_rgba == FC_RGBA_VBGR)
-            load_flags |= FT_LOAD_DEFAULT | FT_LOAD_TARGET_LCD_V;
+            load_flags |= FT_LOAD_TARGET_LCD_V;
         else
-            load_flags |= FT_LOAD_DEFAULT | FT_LOAD_TARGET_NORMAL;
+            load_flags |= FT_LOAD_TARGET_NORMAL;
     }
 
     FcBool fc_embeddedbitmap;
