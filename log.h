17b0eef860b3c799bcaed22ced1519c83908b056
Daniel Eklöf <daniel@ekloef.se>
2021-04-16 20:43:18 +0200

    log: prefix functions and enum types with “fcft_”
    fcft is a library, and it’s a very bad idea to use such generic names,
    as the risk of collisions is too great.
    
    Long term, we should work towards removing logging altogether.

--- log.h
+++ log.h
@@ -1,39 +1,39 @@
 #pragma once
 
-enum log_facility { LOG_FACILITY_USER, LOG_FACILITY_DAEMON };
-enum log_class { LOG_CLASS_ERROR, LOG_CLASS_WARNING, LOG_CLASS_INFO, LOG_CLASS_DEBUG };
+enum fcft_log_facility { FCFT_LOG_FACILITY_USER, FCFT_LOG_FACILITY_DAEMON };
+enum fcft_log_class { FCFT_LOG_CLASS_ERROR, FCFT_LOG_CLASS_WARNING, FCFT_LOG_CLASS_INFO, FCFT_LOG_CLASS_DEBUG };
 
-void log_init(enum log_facility syslog_facility);
-void log_deinit(void);
+void fcft_log_init(enum fcft_log_facility syslog_facility);
+void fcft_log_deinit(void);
 
-void log_msg(enum log_class log_class, const char *module,
-               const char *file, int lineno,
-               const char *fmt, ...) __attribute__((format (printf, 5, 6)));
+void fcft_log_msg(enum fcft_log_class log_class, const char *module,
+                  const char *file, int lineno,
+                  const char *fmt, ...) __attribute__((format (printf, 5, 6)));
 
-void log_errno(enum log_class log_class, const char *module,
-               const char *file, int lineno,
-               const char *fmt, ...) __attribute__((format (printf, 5, 6)));
+void fcft_log_errno(enum fcft_log_class log_class, const char *module,
+                    const char *file, int lineno,
+                    const char *fmt, ...) __attribute__((format (printf, 5, 6)));
 
-void log_errno_provided(
-    enum log_class log_class, const char *module,
+void fcft_log_errno_provided(
+    enum fcft_log_class log_class, const char *module,
     const char *file, int lineno, int _errno,
     const char *fmt, ...) __attribute__((format (printf, 6, 7)));
 
 #define LOG_ERR(...)  \
-    log_msg(LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
+    fcft_log_msg(FCFT_LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_ERRNO(...) \
-    log_errno(LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
+    fcft_log_errno(FCFT_LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_ERRNO_P(_errno, ...)                                        \
-    log_errno_provided(LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, \
-                       _errno, __VA_ARGS__)
+    fcft_log_errno_provided(FCFT_LOG_CLASS_ERROR, LOG_MODULE, __FILE__, __LINE__, \
+                            _errno, __VA_ARGS__)
 #define LOG_WARN(...)  \
-    log_msg(LOG_CLASS_WARNING, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
+    fcft_log_msg(FCFT_LOG_CLASS_WARNING, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
 #define LOG_INFO(...)  \
-    log_msg(LOG_CLASS_INFO, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
+    fcft_log_msg(FCFT_LOG_CLASS_INFO, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
 
 #if defined(LOG_ENABLE_DBG) && LOG_ENABLE_DBG
  #define LOG_DBG(...)  \
-    log_msg(LOG_CLASS_DEBUG, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
+    fcft_log_msg(FCFT_LOG_CLASS_DEBUG, LOG_MODULE, __FILE__, __LINE__, __VA_ARGS__)
 #else
  #define LOG_DBG(...)
 #endif
